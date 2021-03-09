b1a8be75c261898697686a9e2c2f1f9f0f51a27d
ticincloud <44474450+ticincloud@users.noreply.github.com>
2021-03-09 22:30:14 +0100

    Add files via upload

--- restaurants/restaurant.sql
+++ restaurants/restaurant.sql
@@ -12,6 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Dumping database structure for my_app
+CREATE DATABASE IF NOT EXISTS `my_app` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `my_app`;
+
 -- Dumping structure for table my_app.restaurants
 CREATE TABLE IF NOT EXISTS `restaurants` (
   `id_restaurant` int(11) NOT NULL AUTO_INCREMENT,
